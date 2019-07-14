f4ee5bead3238225a289ff0b74bf9e2bbb89b370
Leandro Maroni <leandrodmaroni@gmail.com>
2019-07-14 15:47:31 -0500

    removed whitespaces

--- bikeshare.py
+++ bikeshare.py
@@ -7,10 +7,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-
 def get_filters():
-
-
     """
     Asks user to specify a city, month, and day to analyze.
 
